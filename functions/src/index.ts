aa599bda65f51a5db16c3ac9088a0e46bf7c332c
pedromassango <pedromassango.developer@gmail.com>
2024-10-18 00:36:39 +0200

    fix: push firebase config files

---
+++ functions/src/index.ts
@@ -0,0 +1,45 @@
+import functions = require('firebase-functions');
+import admin = require('firebase-admin');
+
+admin.initializeApp();
+const firestore = admin.firestore();
+//const messaging = admin.messaging();
+const auth = admin.auth();
+
+
+exports.completeAccountCreation = functions.https.onCall(async (data, context) => {
+    // Check if the user is authenticated
+    if (!context.auth) {
+        throw new functions.https.HttpsError(
+            'unauthenticated',
+            'The user must be authenticated to perform this operation.'
+        );
+    }
+
+    try {
+        const uid = data.uid;
+        const appFlavor = data.appFlavor;
+        
+        if (appFlavor != 'pro' && appFlavor != 'salo') {
+            throw new functions.https.HttpsError('failed-precondition','Invalid claim');
+        }
+
+        const docRef = firestore.collection(`${appFlavor}_users`).doc(uid);
+        await docRef.set({
+            firstName: data.firstName,
+            lastName: data.lastName,
+            province: data.province,
+            city: data.city,
+            gender: data.gender,
+            categoryId: data.categoryId,
+            subcategoryIds: data.subcategoryIds,
+        });
+
+        // Set custom claim (e.g., 'pro' or 'salo' role)
+        await auth.setCustomUserClaims(uid, { role: appFlavor });
+        return { message: 'Success' };
+    } catch (error) {
+        console.info(`ERROR: ${JSON.stringify(error)}`);
+        throw new functions.https.HttpsError('internal', `SERVER: ${error}`, error);
+    }
+});
